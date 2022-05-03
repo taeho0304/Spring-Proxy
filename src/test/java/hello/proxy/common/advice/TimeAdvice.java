cb853c2e9a32ec240aac49135ce78891b9addc84
배태호 <taeho.bae@kbfg.com>
2022-05-03 15:30:38 +0900

    CGLIB

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,13 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        return null;
+    }
+}
--- src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -78,4 +78,5 @@ public class ReflectionTest {
             return "B";
         }
     }
+
 }
